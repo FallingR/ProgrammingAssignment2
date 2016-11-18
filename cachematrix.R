a1e4de8497015743d0d3fcb627929a6ca94f401f
stuonly <stuonly@gmail.com>
2016-11-18 10:46:14 -0600

    Matrix  get

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invrs <- NULL
+        set <- function(y) {
+                x <- y
+                invrs <- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) invrs <- inverse
+        getinverse <- function() invrs
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
 }
 
 
